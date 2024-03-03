ba800a4aea5cfa0d1c56057ff24f6c563601c1d6
Anotm <tomanaleksiev101@gmail.com>
2024-03-02 21:20:09 -0500

    Started making Cell

--- script.js
+++ script.js
@@ -15,9 +15,40 @@ class Cell {
 		if (y+1 < mapWidth) {
 			this.adjacent.push([x,y+1]);
 		}
+		console.log(this.adjacent);
 		this.connected = [];
 	}
-	connect (x,y) {
-		
+
+	getRandAdjacecnt() {
+		return this.adjacent[Math.floor(Math.random()*this.adjacent.length)];
+	}
+
+	connect([x,y]) {
+		var contains = false;
+		for (var i = 0; i < this.adjacent.length; i++) {
+			if(this.adjacent[i][0] == x && this.adjacent[i][1] == y) { 
+				var copy = this.adjacent.slice(i);
+				console.log("copy", copy);
+				this.adjacent = copy;
+				contains = true;
+				break;
+			}
+		}
+
+		if (!contains) { return; }
+
+		for (var i = 0; i < this.connected.length; i++) {
+			if(this.connected[i][0] == x && this.connected[i][1] == y) { 
+				return;
+			}
+		}
+
+		this.connected.push([x,y]);
+		console.log(this.connected);
+		console.log("adjacent", this.adjacent);
 	}
 }
+
+const test = new Cell(1,1,5);
+test.connect([1,2]);
+test.connect([1,5]);
