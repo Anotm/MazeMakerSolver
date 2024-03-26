98f0744382d7b6cfd2d91e81e2e4d1096e55131e
Toman Aleksiev <tomanaleksiev101@gmail.com>
2024-03-26 14:25:55 -0400

    Began working on visual logic

--- script.js
+++ script.js
@@ -18,49 +18,72 @@ class Cell {
 		console.log(this.adjacent);
 		this.connected = [];
 		this.mapWidth = mapWidth;
+		this.set = false;
 	}
 
 	getRandAdjacecnt() {
 		return this.adjacent[Math.floor(Math.random()*this.adjacent.length)];
 	}
 
-	connect([x,y]) {
-		var contains = false;
+	isAjacent(x,y) {
 		for (var i = 0; i < this.adjacent.length; i++) {
 			if(this.adjacent[i][0] == x && this.adjacent[i][1] == y) { 
-				var copy = this.adjacent.slice(i);
-				console.log("copy", copy);
-				this.adjacent = copy;
-				contains = true;
-				break;
+				return true;
 			}
 		}
+		return false;
+	}
 
-		if (!contains) { return; }
-
+	isConnected(x,y) {
 		for (var i = 0; i < this.connected.length; i++) {
 			if(this.connected[i][0] == x && this.connected[i][1] == y) { 
-				return;
+				return true;
 			}
 		}
+		return false;
+	}
 
-		this.connected.push([x,y]);
-		console.log(this.connected);
-		console.log("adjacent", this.adjacent);
+	connect(x,y) {
+		if (isAjacent(x,y) && !isConnected(x,y)) {
+			this.connected.push([x,y]);
+		}
+	}
+
+	isSet(){
+		return this.set;
+	}
+
+	drawBorders() {
+		for (var i = 0; i < this.adjacent.length; i++) {
+			cellX = this.adjacent[i][0];
+			cellY = this.adjacent[i][1];
+			
+			if(!isConnected(cellX,cellY)) { 
+				return true;
+			}
+		}
+		return false;
 	}
 }
 
-function makeGrid(gridWidth, cellWidth) {
+function setDimentiosn(gridWidth, cellWidth, borderWidth) {
+	$("div.grid").css({"--grid-w":gridWidth, "--cell-w":cellWidth+"px", "--border-w":borderWidth+"px"});
+}
+
+function makeGrid(gridWidth, cellWidth, borderWidth) {
 	// x = column
 	// y = row
+	$("div.grid").empty();
 	for (var i = 0; i < gridWidth*gridWidth; i++) {
 		$("div.grid").append('<div class="cell" id="x' + i%gridWidth + 'y' + Math.floor(i/gridWidth) + '"></div>');
 	}
-	$("div.grid").css({"--grid-w":gridWidth, "--cell-w":cellWidth});
+	setDimentiosn(gridWidth, cellWidth, borderWidth);
 }
+
+
 
 // const test = new Cell(1,1,5);
 // test.connect([1,2]);
 // test.connect([1,5]);
 
-makeGrid(5,"50px");
+makeGrid(5,50,2);
